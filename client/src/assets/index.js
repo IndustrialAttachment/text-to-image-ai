8a4c9d7e3b903b81cd85a54261a63481d47b3a7a
Mujahidul Islam <mujahidulislamshawn@gmail.com>
2024-12-16 21:34:40 +0600

    Final Work

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import preview from './preview.png';
+import logo from './logo.png';
+import github from './github.svg';
+
+export {
+  download,
+  preview,
+  logo,
+  github
+};
